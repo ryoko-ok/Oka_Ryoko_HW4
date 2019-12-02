1e612d5a0d2f3072b7ad6b087037ac62089808b6
Ryoko Oka <ryooo.kooo.22@gmail.com>
2019-12-02 18:25:52 -0500

    adding py

---
+++ data/men_skiing.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+
+years = [1956, 1972, 1980, 1992, 1994, 1998, 2014]
+
+pops = [1, 3, 1, 3, 8, 7, 8]
+
+plt.plot(years, pops, color=(255/255, 100/255, 100/255), linewidth=6.0)
+
+plt.ylabel("world Population by Billions")
+plt.xlabel("Population Growth by Year")
+plt.title("Global Population Growth 1900-2015", pad=20)
+
+# show the chart
+plt.show()import matplotlib.pyplot as plt
+
+years = [1956, 1972, 1980, 1992, 1994, 1998, 2014]
+
+pops = [1, 3, 1, 3, 8, 7, 8]
+
+plt.plot(years, pops, color=(255/255, 100/255, 100/255), linewidth=6.0)
+
+plt.ylabel("world Population by Billions")
+plt.xlabel("Population Growth by Year")
+plt.title("Global Population Growth 1900-2015", pad=20)
+
+# show the chart
+plt.show()
